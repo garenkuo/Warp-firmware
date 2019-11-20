8a2435c53f309c6bf952992f22646612786ae2c8
garenkuo <kcguo@caltech.edu>
2019-11-20 12:57:50 +0000

    Fix configure def

--- build/ksdk1.1/work/demos/Warp/src/devINA219.h
+++ build/ksdk1.1/work/demos/Warp/src/devINA219.h
@@ -9,7 +9,7 @@ WarpStatus	readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes);
 WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister,
 					uint8_t payloadBtye,
 					uint16_t menuI2cPullupValue);
-WarpStatus	configureSensorINA219(uint8_t payloadF_SETUP, uint8_t payloadCTRL_REG1, uint16_t menuI2cPullupValue);
+WarpStatus	configureSensorINA219(uint16_t menuI2cPullupValue);
 // WarpStatus	readSensorSignalINA219(WarpTypeMask signal,
 // 					WarpSignalPrecision precision,
 // 					WarpSignalAccuracy accuracy,
