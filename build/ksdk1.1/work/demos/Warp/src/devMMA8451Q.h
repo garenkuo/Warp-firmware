07cfa450093cfdfb8114d37c5de3ed2e8e24a3e9
Karen Guo <kg472@physcomplab.eng.cam.ac.uk>
2019-11-22 11:57:56 +0000

    compile

--- build/ksdk1.1/work/demos/Warp/src/devMMA8451Q.h
+++ build/ksdk1.1/work/demos/Warp/src/devMMA8451Q.h
@@ -51,22 +51,3 @@ WarpStatus	readSensorSignalMMA8451Q(WarpTypeMask signal,
 					WarpSignalReliability reliability,
 					WarpSignalNoise noise);
 void		printSensorDataMMA8451Q(bool hexModeFlag);
-
-#ifndef WARP_BUILD_ENABLE_DEVINA219
-#define WARP_BUILD_ENABLE_DEVINA219
-#endif
-
-void		initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile *  deviceStatePointer);
-WarpStatus	readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes);
-WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister,
-					uint8_t payloadBtye,
-					uint16_t menuI2cPullupValue);
-WarpStatus	configureSensorINA219(uint16_t menuI2cPullupValue);
-// WarpStatus	readSensorSignalINA219(WarpTypeMask signal,
-// 					WarpSignalPrecision precision,
-// 					WarpSignalAccuracy accuracy,
-// 					WarpSignalReliability reliability,
-// 					WarpSignalNoise noise);
-void		printSensorCurrentINA219(bool hexModeFlag);
-WarpStatus	readINA219(uint16_t menuI2cPullupValue);
-WarpStatus readSensorCurrentRegisterINA219();
